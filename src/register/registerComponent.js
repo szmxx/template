2f9f52b3039ac97ff3f468f39a7f14710ab85263
Tommy Chen <tommy351@gmail.com>
2021-03-22 14:10:54 +0800

    添加view-design

--- src/register/registerComponent.js
+++ src/register/registerComponent.js
@@ -1,9 +1,13 @@
 /*
  * @Author: weicong
  * @Date: 2021-03-10 17:31:56
- * @LastEditTime: 2021-03-10 17:37:08
+ * @LastEditTime: 2021-03-22 09:18:58
  * @LastEditors: weicong
  * @Description:
  */
 
 import "@/components";
+import Vue from "vue";
+import ViewUI from "view-design";
+import "view-design/dist/styles/iview.css";
+Vue.use(ViewUI);
